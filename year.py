cba965eafd4e7b84076e7dd23d0e6411cba95850
susa555 <47441416+susa555@users.noreply.github.com>
2019-02-08 01:49:08 -0800

    Update year.py

--- year.py
+++ year.py
@@ -1,6 +1,6 @@
-y= int(raw_input())
+s= int(raw_input())
 
-if (( y%400 == 0)or (( y%4 == 0 ) and ( y%100 != 0))):
+if (( s%400 == 0)or (( s%4 == 0 ) and ( s%100 != 0))):
     print("yes")
 else:
     print("no")
