3a84f96f6dfdeeeb715faf4e59d281fdf0c83eae
susa555 <47441416+susa555@users.noreply.github.com>
2019-02-18 22:02:18 -0800

    Create amsinter.py

---
+++ amsinter.py
@@ -0,0 +1,13 @@
+lower,upper=raw_input().split()
+lower=int(lower)
+upper=int(upper)
+for num in range(lower+1,upper):
+    order = len(str(num))
+    sum=0
+    temp=num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if num == sum:
+        print(num)
