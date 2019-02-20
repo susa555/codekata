b41d6f80bf4b16e99789a43985ca8589949ccc2c
susa555 <47441416+susa555@users.noreply.github.com>
2019-02-19 20:23:18 -0800

    Update even num.py

--- even num.py
+++ even num.py
@@ -1,5 +1,5 @@
-lower_limit = int(input(6))
-upper_limit = int(input(9))
+lower_limit = int(input())
+upper_limit = int(input())
 for i in range(lower_limit,upper_limit+1):
   if(i%2 == 0):
     print(i)
