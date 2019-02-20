84438643dc4a0c1060b2604016992ebaed9c87a0
susa555 <47441416+susa555@users.noreply.github.com>
2019-02-19 22:41:15 -0800

    Create primenum.py

---
+++ primenum.py
@@ -0,0 +1,10 @@
+start = 1
+end = 10
+  
+for val in range(start, end + 1): 
+   if val > 1: 
+       for n in range(2, val): 
+           if (val % n) == 0: 
+               break
+       else: 
+           print(val) ,
